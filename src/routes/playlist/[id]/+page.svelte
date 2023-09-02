89dd715f746ef6d3c94f9a9c8fd986c8378656bf
Antar Espadas <antar.espadas@hotmail.com>
2023-09-02 01:45:05 -0600

    Carga promesa únicamente del del cliente

--- src/routes/playlist/[id]/+page.svelte
+++ src/routes/playlist/[id]/+page.svelte
@@ -1,20 +1,29 @@
-{#await getPlaylistItems()}
-	<p>loading...</p>
-{:then [playlistItems, playlistInfo]}
-	<TitleBar title={playlistInfo.snippet.title} />
-	<PlaylistPlayer {playlistItems} />
-{:catch error}
-	<p>{error.message}</p>
-{/await}
+{#if promise}
+	{#await promise}
+		<p>loading...</p>
+	{:then [playlistItems, playlistInfo]}
+		<TitleBar title={playlistInfo.snippet.title} />
+		<PlaylistPlayer {playlistItems} />
+	{:catch error}
+		<p>{error.message}</p>
+	{/await}
+{/if}
 
 <script lang="ts">
 	import type { PageData } from './$types'
 	import TitleBar from '$lib/components/TitleBar.svelte'
 	import PlaylistPlayer from '$lib/components/youtube/PlaylistPlayer.svelte'
 	import type { Item, PlaylistItems } from '$lib/youtube/types'
+	import { onMount } from 'svelte'
 
 	export let data: PageData
 	$: youtubeApi = data.youtbeApi
+
+	let promise: Promise<[PlaylistItems, Item]>
+
+	onMount(() => {
+		promise = getPlaylistItems()
+	})
 
 	async function getPlaylistItems(): Promise<[PlaylistItems, Item]> {
 		const promises = [
