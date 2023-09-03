236fe958de1a91e086bc2b2b2edd17c272b4c363
Antar Espadas <antar.espadas@hotmail.com>
2023-09-02 18:44:27 -0600

    Añade estilos a barra de búsqueda

--- src/lib/components/search/SearchBar.svelte
+++ src/lib/components/search/SearchBar.svelte
@@ -1,9 +1,12 @@
 <form on:submit|preventDefault={(e) => dispatch('search')}>
-	<input type="search" id="search" bind:value={query} />
-	<button type="submit">Cargar</button>
+	<div class="container">
+		<input type="search" id="search" bind:value={query} />
+		<IconButton type="submit" label="buscar" icon="search" iconSize="2rem" />
+	</div>
 </form>
 
 <script lang="ts">
+	import IconButton from '$lib/components/IconButton.svelte'
 	import { createEventDispatcher } from 'svelte'
 
 	export let query = ''
@@ -12,3 +15,32 @@
 		search: void
 	}>()
 </script>
+
+<style lang="sass">
+
+	@import "$lib/styles/breakpoints"
+
+	form
+		width: 100%
+		display: flex
+		justify-content: center
+		align-items: center
+		margin: 2rem 0
+
+	.container 
+		display: grid
+		grid-template-columns: 1fr 40px
+		gap: 0.5rem
+
+		@include xs
+			width: 100%
+		@include sm
+			width: 400px
+
+	input
+		padding: 0.5rem
+		border: 1px solid #ccc
+		border-radius: 0.5rem
+		margin-right: 0.5rem
+
+</style>
